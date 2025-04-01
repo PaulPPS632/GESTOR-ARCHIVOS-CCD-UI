8156b63b5181c587252ae818aaf34916fa4d9b5c
PaulPPS632 <jeffertsap@hotmail.com>
2025-04-01 14:05:50 -0500

    agregando loader a crear usuarios

--- src/app/pages/dashboard/usuarios/usuarios.component.ts
+++ src/app/pages/dashboard/usuarios/usuarios.component.ts
@@ -1,52 +1,95 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ModalComponent } from "../../../components/modal/modal.component";
+import { ModalComponent } from '../../../components/modal/modal.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Usuario } from '../../../interfaces/types';
 import { RouterLink } from '@angular/router';
+import { LoaderComponent } from '../../../components/loader/loader.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [ModalComponent,FormsModule, RouterLink],
+  imports: [ModalComponent, FormsModule, RouterLink, LoaderComponent],
   templateUrl: './usuarios.component.html',
-  styleUrl: './usuarios.component.css'
+  styleUrl: './usuarios.component.css',
 })
-export class UsuariosComponent implements OnInit{
+export class UsuariosComponent implements OnInit {
   modalusuarios: boolean = false;
+  isLoader: boolean = false;
   authservice = inject(AuthService);
   usuario: Usuario | null = null;
-  newUsusario: Usuario ={
+  newUsusario: Usuario = {
     name: '',
     email: '',
     password: '',
-    rolId: 0
+    rolId: 0,
   };
   ListaUsuarios: Usuario[] = [];
+  Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
   ngOnInit(): void {
     this.authservice.usuario$.subscribe((res) => {
       this.usuario = res;
     });
+    this.cargarUsuarios();
+  }
+  cargarUsuarios(){
     this.authservice.listar().subscribe({
-      next: (res) =>{
+      next: (res) => {
         this.ListaUsuarios = res;
       },
-      error: (error) =>{
+      error: (error) => {
         console.log(error);
-      }
+      },
     });
   }
-  tooglemodalusuarios(){
+  tooglemodalusuarios() {
     this.modalusuarios = !this.modalusuarios;
-  } 
-  crear(){
-    if( this.usuario?.rol?.name === 'admin' ){
+  }
+  crear() {
+    if (this.usuario?.rol?.name === 'admin') {
+      this.isLoader = true;
       this.authservice.crear(this.newUsusario).subscribe({
-        next: (res) =>{
+        next: (res) => {
           console.log(res);
+          this.isLoader = false;
+          this.modalusuarios = false;
+          this.cargarUsuarios();
+          this.newUsusario = {
+            name: '',
+            email: '',
+            password: '',
+            rolId: 0,
+          };
+          this.Toast.fire({
+            icon: 'success',
+            title: 'Se creao el usuario correctamente correctamente',
+          });
         },
-        error: (error) =>{
+        error: (error) => {
           console.log(error);
-        }
+          this.isLoader = false;
+          this.modalusuarios = false;
+          this.newUsusario = {
+            name: '',
+            email: '',
+            password: '',
+            rolId: 0,
+          };
+          this.Toast.fire({
+            icon: 'error',
+            title: 'Se creao el usuario correctamente correctamente',
+          });
+        },
       });
     }
   }
