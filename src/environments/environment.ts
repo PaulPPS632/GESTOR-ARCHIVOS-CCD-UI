d21f1dfc7ad1dfccc3ee1c9899e14765adc7a0f2
PaulPPS632 <jeffertsap@hotmail.com>
2025-04-09 14:06:22 -0500

    corrigiendo ruta enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     //API_URL: 'https://aybecoconsultingperu.com/api',
     //API_URL: `https://${window.location.hostname}`,
-    API_URL: `http://${window.location.hostname}:8000/api/gestor-archivos`,
+    API_URL: `https://${window.location.hostname}/api/gestor-archivos`,
     //API_URL: "http://localhost:3000"
     //API_URL: 'https://aybecoconsultingperu.com/api2',
   };
